a91dbaecc339963a624e5873ddaf7ebc5705dd3a
Joseph Cutor <cutormarkjoseph@gmail.com>
2024-09-06 17:51:06 +0800

    fixing bugs for templates

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGO_URI
+const MONGODB_URI = process.env.MONGODB_URI
+const DATABASE_NAME = process.env.DATABASE_NAME
 
 if (!MONGODB_URI) {
 	throw new Error(
@@ -23,7 +24,7 @@ async function dbConnect() {
 			bufferCommands: false,
 		}
 		cached.promise = mongoose.connect(MONGODB_URI,{
-			dbName: 'sampledb',
+			dbName: DATABASE_NAME,
 		}, opts).then(mongoose => {
 			console.log('Db connected')
 			return mongoose
